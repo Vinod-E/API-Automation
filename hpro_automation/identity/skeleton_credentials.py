526a171fbc4684a6b764dfe556768f68f70687f0
Vinod Ashwin <vinod.eraganaboina@hirepro.in>
2024-06-20 12:30:09 +0530

    ignore file update

---
+++ hpro_automation/identity/skeleton_credentials.py
@@ -0,0 +1,68 @@
+server_api = "https:************/py/"
+
+login_details = {"crpo": {"LoginName": '',
+                          "Password": '',
+                          "TenantAlias": '',
+                          "UserName": ''},
+
+                 "ams_crpo": {"LoginName": '',
+                              "Password": '',
+                              "TenantAlias": '',
+                              "UserName": ''},
+
+                 "int": {"LoginName": '',
+                         "Password": '',
+                         "TenantAlias": '',
+                         "UserName": ''},
+
+                 "ams_int": {"LoginName": '',
+                             "Password": '',
+                             "TenantAlias": '',
+                             "UserName": ''},
+
+                 "amsin_non_eu": {"LoginName": '',
+                                  "Password": '',
+                                  "TenantAlias": '',
+                                  "UserName": ''},
+                 "amsin_eu": {"LoginName": '',
+                              "Password": '',
+                              "TenantAlias": '',
+                              "UserName": ''},
+                 "live_non_eu": {"LoginName": '',
+                                 "Password": '',
+                                 "TenantAlias": '',
+                                 "UserName": ''},
+                 "live_eu": {"LoginName": '',
+                             "Password": '',
+                             "TenantAlias": '',
+                             "UserName": ''},
+                 }
+
+db_details = {
+              "crpo_amsin": {"host": '',
+                             "database": '',
+                             "user": '',
+                             "password": ""},
+              "tenant_amsin": {"host": '',
+                               "database": '',
+                               "user": '',
+                               "password": ""},
+              "crpo_ams": {"host": "",
+                           "database": "",
+                           "user": "",
+                           "password": ""},
+              "tenant_ams": {"host": "",
+                             "database": "",
+                             "user": "",
+                             "password": ""}
+              }
+
+replica_db_details = {"amsinr": {"host": '',
+                                 "database": '',
+                                 "user": '',
+                                 "password": ""},
+                      "amsin1r": {"host": '',
+                                  "database": '',
+                                  "user": '',
+                                  "password": ""}
+                      }
